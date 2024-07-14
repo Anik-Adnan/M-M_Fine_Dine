5083c7b65dfebaac4c2601628b74cbb6859358ca
Anik Adnan <anikadnanict17@gmail.com>
2024-07-15 00:50:04 +0600

    food category class created

---
+++ fine_dine/lib/food_category/food_item.dart
@@ -0,0 +1,12 @@
+
+class FoodItem {
+  String name;
+  String imageUrl; // URL or asset path for the food image
+  double price;
+
+  FoodItem({
+    required this.name,
+    required this.imageUrl,
+    required this.price,
+  });
+}
